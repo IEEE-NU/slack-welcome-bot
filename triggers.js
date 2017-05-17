6907601c05e39974a10078ded988157a2af190c6
paep3nguin <paep3nguin@gmail.com>
2017-05-17 00:20:22 -0500

    Some comments in triggers

--- triggers.js
+++ triggers.js
@@ -1,4 +1,7 @@
+// Value: the phrases that trigger a set respons
+// Key: the key of the message (in messages.js) to send in response
 let triggers = {
+    intro: ["intro", "introduction"],
     welcome: ["welcome message"],
     hello: ["hello", "hi", "hey", "sup"],
     code_location: ["show me your code", "code"],
@@ -6,6 +9,8 @@ let triggers = {
 }
 
 // Invert the triggers
+// Keys are now phrases
+// Values are now the key of the message to respond with
 let inverse = {};
 for (const key in triggers) {
     for (let i = 0, l = triggers[key].length; i < l; i++) {
