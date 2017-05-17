600675341ca486f20f5e9bb26a754c70b298115a
paep3nguin <paep3nguin@gmail.com>
2017-05-16 23:45:29 -0500

    Nicer links

--- messages.js
+++ messages.js
@@ -3,7 +3,7 @@ module.exports = {
     code_location: "My code is available for all to see at github.com/ieeE-NU/slack-welcome-bot!",
     welcome: ["Welcome to the IEEE NU Slack! We're happy to have you. " +
         "You can use the #general channel to discuss whatever you want, CS-related or not.",
-        "We recommend using meetfranz.com or the official Slack client so you can have this Slack open without taking up a precious browser tab. " +
+        "We recommend using <meetfranz.com|Franz> or the <slack.com/get|official Slack client> so you can have this Slack open without taking up a precious browser tab. " +
         "Franz even lets you have Messenger, GroupMe, WhatsApp, and many other chat services in one handy client."].join('\n\n'),
     help: "No",
     unauthorized_admin: "Sorry, only admins can use commands (anything starting with a '>'). Admins can be added to the admins.js file in the slack-welcome-bot repo",
