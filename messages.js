72b84cfbf6d8d05ac4e372ff3a6064f7c4fd42ed
paep3nguin <paep3nguin@gmail.com>
2017-05-17 00:23:50 -0500

    More helpful unknown trigger message

--- messages.js
+++ messages.js
@@ -17,6 +17,6 @@ let messages = {
     invalid_path: "Invalid path for Firebase, cannot contain the characters: .$[]/",
     invalid_data: "Invalid data, must be valid JSON",
     unknown_command: "Sorry, I don't recognize that command",
-    unknown_trigger: "Sorry, I'm not sure what you want"
+    unknown_trigger: "Sorry, I'm not sure what you meant. Message back *help* to see what I respond to"
 }
 module.exports = messages;
